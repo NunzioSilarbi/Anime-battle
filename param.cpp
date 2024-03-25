47a3d52a7c35a2611f13f68f21d79ed9a4de901d
nsilarbi <nunzio.silarbi@edu.univ-fcomte.fr>
2024-03-25 19:40:08 +0100

    LA MUSIQUE MARCHE + BOUCLE + EST SELECTIONNABLE

--- param.cpp
+++ param.cpp
@@ -1,6 +1,8 @@
+// param.cpp
+
 #include "param.h"
 #include "ui_param.h"
-#include <QMediaPlayer>
+#include "animebattle.h" // Incluez le fichier d'en-tête d'Animebattle si nécessaire
 
 Param::Param(QWidget *parent)
     : QWidget(parent)
@@ -8,20 +10,77 @@ Param::Param(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Define the file name variable
-    QString musicFileName = "Overdrive.mp3";
+    // Ajoutez vos musiques à la liste déroulante
+    ui->comboBox_music->addItem("Music 1");
+    ui->comboBox_music->addItem("Music 2");
+    ui->comboBox_music->addItem("Music 3");
 
-    // Create a QMediaPlayer object
-    QMediaPlayer *player = new QMediaPlayer;
+    // Connectez le signal activated du QComboBox à la fonction onMusicChanged
+    connect(ui->comboBox_music, QOverload<int>::of(&QComboBox::activated), this, &Param::onMusicChanged);
 
-    // Set the media source to the background music file using the variable
-    player->setMedia(QUrl("qrc:/static/Music/" + musicFileName));
+    // Initialisez le lecteur multimédia
+    player = new QMediaPlayer(this);
 
-    // Play the background music
-    player->play();
+    // Jouez automatiquement la musique "Wildfire" au lancement de l'application
+    playMusic("Music 1");
+
+    // Connectez le signal clicked du bouton "Go Back" à la fonction goBack
+    connect(ui->button_param_back, &QPushButton::clicked, this, &Param::goBack);
 }
 
 Param::~Param()
 {
     delete ui;
+}
+
+void Param::onMusicChanged(int index)
+{
+    // Récupérez le nom de la musique sélectionnée
+    QString musicName = ui->comboBox_music->currentText();
+
+    // Faites quelque chose avec le nom de la musique sélectionnée (par exemple, jouer la musique)
+    playMusic(musicName);
+}
+
+void Param::playMusic(const QString &musicName)
+{
+    // Définissez le chemin de la musique en fonction de son nom
+    QString musicFileName;
+
+    if (musicName == "Music 1") {
+        musicFileName = "Wildfire.mp3";
+    } else if (musicName == "Music 2") {
+        musicFileName = "Mass_Destruction.mp3";
+    } else if (musicName == "Music 3") {
+        musicFileName = "Get_Wild.mp3";
+    }
+
+    // Jouez la musique
+    player->setMedia(QUrl("qrc:/static/Music/" + musicFileName));
+
+    // Connectez le signal endOfMedia à la fonction pour relancer la musique une fois terminée
+    connect(player, &QMediaPlayer::mediaStatusChanged, this, &Param::onMediaStatusChanged);
+
+    player->play();
+}
+
+void Param::onMediaStatusChanged(QMediaPlayer::MediaStatus status)
+{
+    if (status == QMediaPlayer::EndOfMedia) {
+        // Relancer la musique
+        player->setPosition(0);
+        player->play();
+    }
+}
+
+void Param::goBack()
+{
+    // Instanciez la fenêtre Animebattle
+    Animebattle *animeBattle = new Animebattle();
+
+    // Affichez la fenêtre Animebattle
+    animeBattle->show();
+
+    // Fermez la fenêtre Param
+    this->close();
 }
--- param.h
+++ param.h
@@ -1,12 +1,13 @@
+// param.h
+
 #ifndef PARAM_H
 #define PARAM_H
 
 #include <QWidget>
+#include <QMediaPlayer>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class Param;
-}
+namespace Ui { class Param; }
 QT_END_NAMESPACE
 
 class Param : public QWidget
@@ -17,8 +18,15 @@ public:
     Param(QWidget *parent = nullptr);
     ~Param();
 
+private slots:
+    void onMusicChanged(int index);
+    void playMusic(const QString &musicName);
+    void onMediaStatusChanged(QMediaPlayer::MediaStatus status);
+    void goBack(); // Nouveau slot pour retourner à Animebattle
+
 private:
     Ui::Param *ui;
+    QMediaPlayer *player;
 };
 
 #endif // PARAM_H

3a090d5123094c26ba13cbf8c1596bd84d58fc59
nsilarbi <nunzio.silarbi@edu.univ-fcomte.fr>
2024-03-25 15:40:22 +0100

    icone valide

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/static/Ico/ABLogo.ico"));
     Animebattle w;
     w.show();
     return a.exec();
 }
+
